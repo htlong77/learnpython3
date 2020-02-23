080cf0d2801933bc3bd71f70ff1986007e13c6c7
Ha Thuy Long <htlong77@gmail.com>
2020-02-23 21:13:30 +0700

    finished all variables declarations for ex4.py

--- ex4.py
+++ ex4.py
@@ -1,1 +1,10 @@
 cars = 100
+space_in_a_car = 4.0
+drives = 30
+passengers = 90
+cars_not_driven = cars - drives
+cars_driven = drives
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
