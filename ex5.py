0f5387fdb6936560357e0139c637ffe5a8e79783
Hà Thụy Long <htlong77@gmail.com>
2020-02-24 10:22:00 +0700

    1st version of ex5.py

---
+++ ex5.py
@@ -0,0 +1,9 @@
+my_name = 'Ha Thuy Long'
+my_age = 43
+my_height = 162
+my_weight = 72
+my_eye = 'Brown'
+my_teeth = 'Black'
+my_hair = 'White'
+
+#print(f"Let's talk about {my_name}.") #Does not work under python3.5
