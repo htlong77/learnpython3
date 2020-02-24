9ea37a80d1eb15b33a4bab13ef62b2376615b619
Ha Thuy Long <htlong77@gmail.com>
2020-02-24 10:40:49 +0700

    ex5.py modified by laptop

--- ex5.py
+++ ex5.py
@@ -2,8 +2,19 @@ my_name = 'Ha Thuy Long'
 my_age = 43
 my_height = 162
 my_weight = 72
-my_eye = 'Brown'
+my_eyes = 'Brown'
 my_teeth = 'Black'
 my_hair = 'White'
 
-#print(f"Let's talk about {my_name}.") #Does not work under python3.5
+print(f"Let's talk about {my_name}.") #Does not work under python3.5
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is stricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+
