7d3a65c39b2949ee454cdc9c9c8c579ef11de432
Ha Thuy Long <hathuylong@hus.edu.vn>
2020-02-29 17:42:34 +0700

    ex.10 from vm-deell7350

---
+++ ex10.py
@@ -0,0 +1,20 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+bel_cat = "I'm a\a bel cat"
+backspace_cat = "I'm a\b backspace cat"
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(bel_cat)
+print(backspace_cat)
+
