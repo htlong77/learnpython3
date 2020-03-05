fa3235ceeb094ef7b330e150155816de3a1422b0
Hà Thụy Long <htlong77@gmail.com>
2020-03-05 22:17:55 +0700

    exercises 20 extended

--- ex20.py
+++ ex20.py
@@ -21,7 +21,7 @@ print("Now let's rewind, kind of like a tape.")
 
 rewind(current_file)
 
-print("Let's print three lines:")
+print("Let's print four lines:")
 
 current_line = 1
 print_a_line(current_line, current_file)
@@ -32,3 +32,9 @@ print_a_line(current_line, current_file)
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
+current_line += 1
+print_a_line(current_line, current_file)
+
+x = []
+x += 'something Hà Thụy Long'
+print(x)
